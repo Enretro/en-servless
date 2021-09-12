2598d2be9eaebcd8a9e2eb96937576a6664f4718
Enretro <envpogled@gmail.com>
2021-09-12 16:44:40 +0200

    weather app

--- README.txt
+++ README.txt
@@ -40,3 +40,19 @@ Build & Deploy/Environment/Edit Variables
 
 AIRTABLE_API_KEY=???????????????
 
+##############################
+
+AIRTABLE
+user: enTruth
+envpogled@gmail.com
+kN4wv58i8nkADLL@
+kN4wv58i8nkADLL@
+
+##############################
+
+OPENWEATHER
+
+envpogled@gmail.com
+lB8ed67p9lfzQWJJ@
+
+http://api.openweathermap.org/data/2.5/weather?q=chicago&appid=fb3d48fdb81553377153b3045221be85&units=metric
--- examples/5-weather/app.js
+++ examples/5-weather/app.js
@@ -5,3 +5,36 @@ const input = document.querySelector('.form-input')
 const alert = document.querySelector('.alert')
 const result = document.querySelector('.result')
 alert.style.display = 'none'
+
+form.addEventListener('submit', (event)=> {
+  event.preventDefault()
+  const city = input.value
+  if(city) {
+    getWeatherData(city)
+  }
+})
+
+async function getWeatherData(city) {
+  alert.style.display = 'none'
+  try {
+    const {data} = await axios.post('/api/5-weather', {city})
+    const {name} = data
+    const {country} = data.sys
+    const {temp_max:max, temp_min:min, feels_like} = data.main
+    const {description} = data.weather[0]
+
+    result.innerHTML = `
+      <article class="card">
+      <h3>${name}, ${name}</h3>
+      <p>${description}</p>
+      <p>min temp : ${min}&#8451</p>
+      <p>max temp : ${max}&#8451</p>
+      <p>feels like : ${feels_like}&#8451</p>
+      </article>
+    `
+  } catch (error) {
+    // console.log(error.response);
+    alert.style.display = 'block'
+    alert.textContent = `Can not find weather for city : "${city}"`
+  }
+}
